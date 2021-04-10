851761b6e5ef35e76dd28c00e54ede043b06badd
Ryan Doyle <ryan@rdoyle.net>
2021-04-09 23:18:42 -0700

    Adding prettiness

--- doyle.sh
+++ doyle.sh
@@ -4,44 +4,71 @@
 # ===========
 # Global Vars
 # ===========
-
 FIRSTNAME="Ryan"
 LASTNAME="Doyle"
-OCCUPATION="Software Engineer"
+OCCUPATION="Infrastructre and Software Engineer"
 LOCATION="the San Francisco Bay Area"
 
-LANGUAGES="golang python bash java javascript"
+LANGUAGES="terraform golang python bash typescript postgresql"
 EMAIL="ryan@rdoyle.net"
+
+# ===============
+# Calculated Vars
+# ===============
+start_unix=1350309600 # Began working in software in 2012
+now_unix=$(date +%s)
+years=$(expr $now_unix - $start_unix)
+for div in 60 60 24 365; do
+    years=$(expr $years / $div)
+done
+# Round off b/c integer math and why not ;)
+years=$(expr $years + 1)
 
 # ===========
 # Intro blurb
 # ===========
 intro(){
+    clear
+    echo;echo;echo
     cat << EOF
-Hello, My name is $FIRSTNAME $LASTNAME.
-I am a $OCCUPATION currently
+   ______              __            __   
+  / ____ \\   _________/ /___  __  __/ /__ 
+ / / __ \\/  / ___/ __  / __ \\/ / / / / _ \\
+/ / /_/ /  / /  / /_/ / /_/ / /_/ / /  __/
+\ \\__,_/  /_/   \\__,_/\\____/\\__, /_/\\___/ 
+ \\____/                    /____/         
+EOF
+	echo 
+    cat << EOF
+Hello, My name is $FIRSTNAME.
+I am an $OCCUPATION currently
 working in $LOCATION.
 EOF
     echo
-    start_unix=1350309600 # Began working in software in 2012
-    now_unix=$(date +%s)
-    years=$(expr $now_unix - $start_unix)
-    for div in 60 60 24 365; do 
-        years=$(expr $years / $div)
-    done
-    # Round off b/c integer math and why not ;)
-    years=$(expr $years + 1) 
-    
-    cat << EOF
-I have been working in large-scale software
-and web infrastructure for the past $years years.
 
-I am proficient in a large variety of programming 
-languages. The ones I prefer most include:
+    cat << EOF
+I have been working in large-scale software and web infrastructure
+for the past $years years.  I'm an avid supporter of open-source
+development and am particularly interested in DevOps and dev strategies
+in general.
+
+I am proficient in a variety of programming languages and tools. The ones I
+favor most include:
 EOF
     for lang in $LANGUAGES; do
         echo " - $lang"
     done
+    echo
+
+    cat << EOF
+And finally some unsolicited advice:
+1. Don't blindly run random scripts you find on the internet (shame 🔔).
+2. Avoid turning your workstation into critical infra.
+3. Automate anything and everything.
+
+https://rdoyle.net
+EOF
+    echo;echo;echo
 }
 
 # If no args passed, default to intro
